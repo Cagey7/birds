4c75e328eae85d0a89dac3939154b3e2acf3f445
Cagey7 <dagodecagey@gmail.com>
2022-12-31 18:48:26 +0600

    Added python-dotenv. Now app load env vars from file

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
 class Config:
     """Class with common configurations for all applications"""
-    SECRET_KEY = "very very hard to guess secret key"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SESSION_TYPE = "filesystem"
     SESSION_PERMANENT = False
 
@@ -13,13 +18,13 @@ class Config:
 class DevelopmentConfig(Config):
     """Devolopment configuration class"""
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///C:\\home\\birds\\databases\\development.sqlite"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_SQLALCHEMY_DATABASE_URI")
 
 
 class ProductionConfig(Config):
     """Production configuration class"""
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:///C:\\home\\birds\\databases\\production.sqlite"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("PROD_SQLALCHEMY_DATABASE_URI")
 
 
 config = {
