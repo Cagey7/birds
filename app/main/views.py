7baa4708ca617d26cabfefb50e9325f559ba91f3
Cagey7 <dagodecagey@gmail.com>
2023-01-06 13:46:53 +0600

    Added voting feature for only logged users

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,19 @@
-from flask import render_template, session, abort
+from flask import render_template, session, abort, request
 from . import main
+from app.models import *
 
 @main.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    birds = None
+    voted_bird = None
+    if session["username"]:
+        user_id = User.query.filter_by(username=session["username"]).first().id
+        bird = Vote.query.filter_by(user_id=user_id).first()
+        if bird:
+            bird_id = bird.bird_id
+            voted_bird = Bird.query.filter_by(id=bird_id).first()
+    birds = Bird.query.all()
+    return render_template("index.html", birds=birds, voted_bird=voted_bird)
 
 
 @main.route("/profile/<username>")
@@ -11,3 +21,14 @@ def profile(username):
     if not username == session["username"]:
         abort(404)
     return render_template("profile.html")
+
+
+@main.route("/vote", methods=["GET", "POST"])
+def vote():
+    if request.form.get("vote"):
+        user_id = User.query.filter_by(username=session["username"]).first().id
+        bird_id = request.form.get("vote")
+        vote = Vote(user_id=user_id, bird_id=bird_id)
+        db.session.add(vote)
+        db.session.commit()
+    return render_template("success.html")
